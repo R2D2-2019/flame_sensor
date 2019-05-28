5b524f83dd29e364d129b2eb0a8020c42205e666
Renske Kuip <renske.kuip@student.hu.nl>
2019-05-28 19:53:20 +0200

    Added doxygen

--- code/headers/flame_sensor.hpp
+++ code/headers/flame_sensor.hpp
@@ -10,16 +10,28 @@ namespace r2d2::flame_sensor {
         hwlib::pin_oc &ir_led_3;
         hwlib::pin_oc &ir_led_4;
         hwlib::pin_oc &ir_led_5;
+        hwlib::pin_oc *leds[5] = {&ir_led_1, &ir_led_2, &ir_led_3, &ir_led_4,
+                                  &ir_led_5};
+
     public:
         flame_sensor_c(hwlib::pin_oc &ir_led_1, hwlib::pin_oc &ir_led_2,
                        hwlib::pin_oc &ir_led_3, hwlib::pin_oc &ir_led_4,
                        hwlib::pin_oc &ir_led_5);
-
-
-        void read_output();
-
+        /**
+         * @brief Check if sensor detects a flame anywhere near
+         *
+         *
+         * @return true if one or more of the leds detect a flame
+         * @return false if no flames are detected
+         */
         bool is_flame_detected();
 
+        /**
+         * @brief Get the flame direction
+         * Currently no implementation
+         *
+         * @return int position of flame in degrees (from sensor)
+         */
         int get_flame_direction();
     };
 } // namespace r2d2::flame_sensor
--- code/src/flame_sensor.cpp
+++ code/src/flame_sensor.cpp
@@ -1,11 +1,11 @@
 #include <flame_sensor.hpp>
 
 namespace r2d2::flame_sensor {
-    flame_sensor_c::flame_sensor_c(hwlib::pin_oc &ir_led_1,
-                                   hwlib::pin_oc &ir_led_2 = hwlib::pin_oc_dummy,
-                                   hwlib::pin_oc &ir_led_3 = hwlib::pin_oc_dummy,
-                                   hwlib::pin_oc &ir_led_4 = hwlib::pin_oc_dummy,
-                                   hwlib::pin_oc &ir_led_5 = hwlib::pin_oc_dummy)
+    flame_sensor_c::flame_sensor_c(
+        hwlib::pin_oc &ir_led_1, hwlib::pin_oc &ir_led_2 = hwlib::pin_oc_dummy,
+        hwlib::pin_oc &ir_led_3 = hwlib::pin_oc_dummy,
+        hwlib::pin_oc &ir_led_4 = hwlib::pin_oc_dummy,
+        hwlib::pin_oc &ir_led_5 = hwlib::pin_oc_dummy)
         : ir_led_1(ir_led_1),
           ir_led_2(ir_led_2),
           ir_led_3(ir_led_3),
@@ -13,14 +13,10 @@ namespace r2d2::flame_sensor {
           ir_led_5(ir_led_5) {
     }
 
-    void flame_sensor_c::read_output() {
-       
-    }
-
     bool flame_sensor_c::is_flame_detected() {
-        hwlib::pin_oc* leds[5] = {&ir_led_1, &ir_led_2, &ir_led_3, &ir_led_4, &ir_led_5};
-        for (int i=0; i<5;++i) {
-            if (leds[i]->read() == 1){
+        size_t size = sizeof(leds) / sizeof(leds[0]);
+        for (size_t i = 0; i < size; ++i) {
+            if (leds[i]->read() == 1) {
                 return 1;
             }
         }
