36af7255f448b781e8c80920e00befae8a201607
Rick van Sambeek <rickvansambeek@gmail.com>
2019-06-28 11:39:40 +0200

    seperated example controller to its own file for a more clean main

---
+++ code/headers/example_controller.hpp
@@ -0,0 +1,45 @@
+ class test_module : public r2d2::base_module_c {
+ public:
+	/**
+	 * The constructor initializes:
+	 * base_module_c
+	 *
+	 * @param comm
+	 */
+	test_module(r2d2::base_comm_c &comm)
+			: base_module_c(comm) {
+			comm.listen_for_frames({r2d2::frame_type::FLAME_DETECTION});
+	}
+
+	/**
+	 * Let the module process data.
+	 */
+	void process() override {
+		auto led = hwlib::target::pin_out(hwlib::target::pins::d13);
+		led.write(true);
+		comm.request(r2d2::frame_type::FLAME_DETECTION);
+
+		while (comm.has_data()) {
+			auto frame = comm.get_data();
+
+			// This module doesn't handle requests
+			if (!frame.request) {
+				const auto flame_detected = frame.as_frame_type<
+						r2d2::frame_type::FLAME_DETECTION
+				>().flame_detected;
+
+				const auto flame_angle = frame.as_frame_type<
+						r2d2::frame_type::FLAME_DETECTION
+				>().flame_angle;
+
+				if (flame_detected) {
+					hwlib::cout << flame_angle << hwlib::endl;
+				} else {
+					hwlib::cout << "no flames" << hwlib::endl;
+				}
+			}
+		}
+		hwlib::wait_ms(10);
+		led.write(false);
+	}
+};
--- code/main.cpp
+++ code/main.cpp
@@ -2,53 +2,8 @@
 #include <hwlib.hpp>
 #include <mock_bus.hpp>
 #include <module.hpp>
+#include <example_controller.hpp>
 
-// class test_module : public r2d2::base_module_c {
-// public:
-//	/**
-//	 * The constructor initializes:
-//	 * base_module_c
-//	 * the pointer to the flame_sensor
-//	 *
-//	 * @param comm
-//	 */
-//	test_module(r2d2::base_comm_c &comm)
-//			: base_module_c(comm) {
-//			comm.listen_for_frames({r2d2::frame_type::FLAME_DETECTION});
-//	}
-//
-//	/**
-//	 * Let the module process data.
-//	 */
-//	void process() override {
-//		auto led = hwlib::target::pin_out(hwlib::target::pins::d13);
-//		led.write(true);
-//		comm.request(r2d2::frame_type::FLAME_DETECTION);
-//
-//		while (comm.has_data()) {
-//			auto frame = comm.get_data();
-//
-//			// This module doesn't handle requests
-//			if (!frame.request) {
-//				const auto flame_detected = frame.as_frame_type<
-//						r2d2::frame_type::FLAME_DETECTION
-//				>().flame_detected;
-//
-//				const auto flame_angle = frame.as_frame_type<
-//						r2d2::frame_type::FLAME_DETECTION
-//				>().flame_angle;
-//
-//				if (flame_detected) {
-//					hwlib::cout << flame_angle << hwlib::endl;
-//				} else {
-//					hwlib::cout << "no flames" << hwlib::endl;
-//				}
-//			}
-//		}
-//		hwlib::wait_ms(10);
-//		led.write(false);
-//	}
-//};
 
 int main(void) {
     // kill the watchdog
